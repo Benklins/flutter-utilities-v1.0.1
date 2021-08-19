d4cb847434c4619bcf88827032c806859b8ac450
JasperEssienDro <jessien@drohealth.com>
2021-08-18 18:06:01 -0700

    added mock shared pref

--- lib/storage_utility/shared_pref/storage_helper.dart
+++ lib/storage_utility/shared_pref/storage_helper.dart
@@ -1,4 +1,7 @@
+import 'package:mocktail/mocktail.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+class MockSharedPreference extends Mock implements SharedPreferences {}
 
 class StorageHelper {
   StorageHelper._();
