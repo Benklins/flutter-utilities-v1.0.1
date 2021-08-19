0febf327ffdd9b4ff05ab4eddf8fd124a2b4bca6
JasperEssienDro <jessien@drohealth.com>
2021-08-19 10:01:23 -0700

    added a compulsory list base viewmodel to be passed when BaseListCubit is created

--- lib/cubit_utility/base_list_cubit.dart
+++ lib/cubit_utility/base_list_cubit.dart
@@ -1,12 +1,13 @@
-
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_utilities/viewmodel_utility/viewmodel_utility_export.dart';
 
 part 'base_list_state.dart';
 
 abstract class BaseListCubit<T> extends Cubit<BaseListState> {
+  final ListBaseViewModel<T> viewModel;
 
-  BaseListCubit() : super(BaseListInitial());
+  BaseListCubit(this.viewModel) : super(BaseListInitial());
 
   void emitListLoaded(List<T> list) {
     emit(BaseListLoaded(list));
