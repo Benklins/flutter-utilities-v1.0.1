add2a98f83faaf047cf152d114b98c33ed4a8272
JasperEssienDro <jessien@drohealth.com>
2021-08-21 00:26:09 -0700

    maded handle_request_response.dart a generic method and class so as to return a specif successresponse rather than a dynamic SuccessResponse

--- lib/request_utility/http_helper/handle_request_response.dart
+++ lib/request_utility/http_helper/handle_request_response.dart
@@ -1,15 +1,15 @@
 import 'package:flutter_utilities/utilities.dart';
 
-class HandleRequestResponse {
+class HandleRequestResponse<T> {
   HandleRequestResponse._();
 
-  static Future<void> handleResponse(
-    Future<SuccessResponse> Function() requestFunc,
+  static Future<void> handleResponse<T>(
+    Future<SuccessResponse<T>> Function() requestFunc,
     void responseFunc(SuccessResponse response),
     void errorFunc(String error),
   ) async {
     try {
-      SuccessResponse response = await requestFunc();
+      SuccessResponse<T> response = await requestFunc();
       responseFunc(response);
     } on RequestFailedException catch (e) {
       var error = (e.message as ErrorResponse);
